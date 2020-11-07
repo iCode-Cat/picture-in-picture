187bed4323e9f230f5785e83fda46f8b195c73c1
iCode-Cat <devranboyac8@gmail.com>
2020-11-07 12:34:34 +0200

    Ener

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert(`Test`)
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media.
+async function selectMediaStream(){
+
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloa = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        //Catch error here
+        console.log(error);
+    }
+
+}
+selectMediaStream()
